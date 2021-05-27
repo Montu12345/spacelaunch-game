73c5e002ebfaa3bc9a1e06248e7ff0cfebf380a9
Montu12345 <shevali@mercurytrip.com>
2021-05-27 02:52:25 -0700

    more cleanup because ian said so

---
+++ include/game_build.h
@@ -0,0 +1,97 @@
+#ifndef __GAME_BUILD_H__
+#define __GAME_BUILD_H__
+
+#include "body.h"
+#include "collision.h"
+#include "color.h"
+#include "forces.h"
+#include "scene.h"
+#include "sdl_wrapper.h"
+#include "sprite.h"
+#include "collision.h"
+#include "spacelaunch2.h"
+
+const int GB_SCREEN_SIZE_X;
+const int GB_SCREEN_SIZE_Y;
+
+const int GB_MAX_OBSTACLES_SCREEN_SIZE_X;
+const int GB_MAX_OBSTACLES_SCREEN_SIZE_Y;
+
+const double GB_ASTEROID_MASS;
+const rgb_color_t GB_BAD_ASTEROID_COLOR;
+const rgb_color_t GB_GOOD_ASTEROID_COLOR;
+const int GB_INITIAL_ASTEROIDS;
+const int GB_ASTEROID_RADIUS;
+
+const rgb_color_t GB_BACKGROUND_COLOR;
+
+const int GB_DISTANCE_BETWEEN_STARS;
+const double GB_FREQUENCY_FOR_STARS;
+const rgb_color_t GB_STAR_COLOR;
+const int GB_STAR_NUM_OF_POINTS;
+const int GB_STAR_MIN_LENGTH;
+const int GB_STAR_MAX_LENGTH;
+
+const vector_t GB_ROCKET_INITIAL_POS;
+const int GB_ROCKET_RADIUS;
+const double GB_ROCKET_MASS;
+const rgb_color_t GB_ROCKET_COLOR;
+
+const int GB_SHOOTING_STAR_RADIUS;
+const int GB_SHOOTING_STAR_MASS;
+const rgb_color_t GB_SHOOTING_STAR_COLOR;
+const vector_t GB_SHOOTING_STAR_VELCOITY;
+
+/**
+ * Makes a shooting star. 
+ *
+ * @param scene a pointer to a scene returned from scene_init()
+ */
+void game_build_shooting_star(scene_t *scene);
+
+/**
+ * Draws the asteroids.
+ *
+ * @param scene a pointer to a scene returned from scene_init()
+ * @param rocket a pointer to the rocket in the scene
+ */
+void game_build_draw_asteroids(scene_t *scene, body_t *rocket);
+
+/**
+ * Draws the background and the stars.
+ *
+ * @param scene a pointer to a scene returned from scene_init()
+ */
+void game_build_make_stary_night(scene_t *scene);
+
+/**
+ * Creates the rocket.
+ * Return: pointer to the rocket
+ * 
+ * @param scene a pointer to a scene returned from scene_init()
+ */
+body_t *game_build_make_rocket(scene_t *scene);
+
+/**
+ * Creates the sky.
+ *
+ * @param scene a pointer to a scene returned from scene_init()
+ */
+void game_build_make_sky(scene_t *scene);
+
+/**
+ * Creates the stars that are in the sky.
+ *
+ * @param scene a pointer to a scene returned from scene_init()
+ */
+void game_build_make_stars(scene_t *scene);
+
+/**
+ * Makes the asteroids.
+ *
+ * @param scene a pointer to a scene returned from scene_init()
+ * @param rocket a pointer to the rocket in the scene.
+ */
+void game_build_make_asteroid(scene_t *scene, body_t *rocket);
+
+#endif // #ifndef __GAME_BUILD_H__
--- library/game_build.c
+++ library/game_build.c
@@ -13,14 +13,13 @@
 #include "sdl_wrapper.h"
 #include "sprite.h"
 #include "collision.h"
-#include "spacelaunch2.h"
+#include "game_build.h"
 
 const int GB_SCREEN_SIZE_X = 1000;
 const int GB_SCREEN_SIZE_Y = 500;
 
 const int GB_MAX_OBSTACLES_SCREEN_SIZE_X = 2000;
 const int GB_MAX_OBSTACLES_SCREEN_SIZE_Y = 1000;
-
 
 const double GB_ASTEROID_MASS = INFINITY;
 const rgb_color_t GB_BAD_ASTEROID_COLOR = {.r = 0, .g = 0, .b = 0};
@@ -64,12 +63,13 @@ enum body_type_t *game_build_body_type_init(enum body_type_t b)
     return body_type;
 }
 
-void game_build_shooting_star(scene_t *scene){
+void game_build_shooting_star(scene_t *scene)
+{
     list_t *shooting_star_list = sprite_make_circle(GB_SHOOTING_STAR_RADIUS);
     body_t *shooting_star = body_init_with_info(shooting_star_list, 
                                                 GB_SHOOTING_STAR_MASS, 
                                                 GB_SHOOTING_STAR_COLOR, 
-                                                body_type_init_2(star_t), 
+                                                game_build_body_type_init(star_t), 
                                                 free);
     vector_t pos = {.x = 0, .y = rand() % GB_MAX_OBSTACLES_SCREEN_SIZE_X};
     vector_t velocity = GB_SHOOTING_STAR_VELCOITY;
@@ -78,32 +78,36 @@ void game_build_shooting_star(scene_t *scene){
     scene_add_body(scene, shooting_star);
 }
 
-void game_build_draw_asteroids(scene_t *scene, body_t *ROCKET){
+void game_build_draw_asteroids(scene_t *scene, body_t *rocket)
+{
     for (int i = 0; i < GB_INITIAL_ASTEROIDS; i++){
-        make_asteroid(scene, ROCKET);
+        make_asteroid(scene, rocket);
     }
 }
 
-void game_build_make_stary_night(scene_t *scene){
-    game_build_make_background_color_2(scene);
-    game_build_make_background_stars_2(scene);
+void game_build_make_stary_night(scene_t *scene)
+{
+    game_build_make_background_color(scene);
+    game_build_make_background_stars(scene);
 }
 
-body_t *game_build_make_rocket(scene_t *scene){
-    list_t *ROCKET_shape = sprite_make_ROCKET(GB_ROCKET_RADIUS);
-    body_t *ROCKET = body_init_with_info(ROCKET_shape, 
+body_t *game_build_make_rocket(scene_t *scene)
+{
+    list_t *rocket_shape = sprite_make_ROCKET(GB_ROCKET_RADIUS);
+    body_t *rocket = body_init_with_info(rocket_shape, 
                                          GB_ROCKET_MASS, 
                                          GB_ROCKET_COLOR, 
-                                         body_type_init_2(rocket_t), 
+                                         game_build_body_type_init(rocket_t), 
                                          free);
-    body_set_centroid(ROCKET, GB_ROCKET_INITIAL_POS);
-    body_set_movable(ROCKET, true);
+    body_set_centroid(rocket, GB_ROCKET_INITIAL_POS);
+    body_set_movable(rocket, true);
     // body_set_camera_mode(ROCKET, FOLLOW);
-    scene_add_body(scene, ROCKET);
-    return ROCKET;
+    scene_add_body(scene, rocket);
+    return rocket;
 }
 
-void game_build_make_sky(scene_t *scene){
+void game_build_make_sky(scene_t *scene)
+{
     list_t *background_list = sprite_make_rect(0, 
                                                GB_MAX_OBSTACLES_SCREEN_SIZE_X, 
                                                0, 
@@ -111,12 +115,13 @@ void game_build_make_sky(scene_t *scene){
     body_t *background = body_init_with_info(background_list, 
                                              INFINITY, 
                                              GB_BACKGROUND_COLOR, 
-                                             body_type_init_2(background_t), 
+                                             game_build_body_type_init(background_t), 
                                              free);
     scene_add_body(scene, background);
 }
 
-void game_build_make_stars(scene_t *scene){
+void game_build_make_stars(scene_t *scene)
+{
     
     for (int i = 0; i < GB_MAX_OBSTACLES_SCREEN_SIZE_Y / GB_FREQUENCY_FOR_STARS; i++){
         for (int j = 0; j < GB_MAX_OBSTACLES_SCREEN_SIZE_X / GB_FREQUENCY_FOR_STARS; j++){
@@ -126,7 +131,7 @@ void game_build_make_stars(scene_t *scene){
             body_t *star = body_init_with_info(star_list, 
                                                INFINITY, 
                                                GB_STAR_COLOR, 
-                                               body_type_init_2(star_t), 
+                                               game_build_body_type_init(star_t), 
                                                free);
             vector_t pos = {.x = i * GB_DISTANCE_BETWEEN_STARS, 
                             .y = j * GB_DISTANCE_BETWEEN_STARS + (i % 2) * GB_DISTANCE_BETWEEN_STARS/2.0};
@@ -136,25 +141,25 @@ void game_build_make_stars(scene_t *scene){
     }
 }
 
-void game_build_make_asteroid(scene_t *scene, body_t *ROCKET)
+void game_build_make_asteroid(scene_t *scene, body_t *rocket)
 {
     list_t *circle = sprite_make_circle(GB_ASTEROID_RADIUS);
     rgb_color_t color;
     enum body_type_t *obstacle_type;
     if (rand() % 2 == 0){
         color = GB_BAD_ASTEROID_COLOR;
-        obstacle_type = body_type_init_2(good_obstacle_t);
+        obstacle_type = game_build_body_type_init(good_obstacle_t);
     }
     else{
         color = GB_GOOD_ASTEROID_COLOR;
-        obstacle_type = body_type_init_2(bad_obstacle_t);
+        obstacle_type = game_build_body_type_init(bad_obstacle_t);
     }
-    body_t *ASTEROID = body_init_with_info(circle, GB_ASTEROID_MASS, color, obstacle_type, free);
+    body_t *asteroid = body_init_with_info(circle, GB_ASTEROID_MASS, color, obstacle_type, free);
     vector_t position = {.x = (rand() % GB_SCREEN_SIZE_X), .y = rand() % GB_SCREEN_SIZE_Y};
-    body_set_centroid(ASTEROID, position);
-    body_set_movable(ASTEROID, false);
+    body_set_centroid(asteroid, position);
+    body_set_movable(asteroid, false);
     // body_set_camera_mode(ASTEROID, SCENE);
     // create_gravity_rocket_obstacles(scene, ROCKET, ASTEROID);
-    create_collision_rocket_obstacles_2(scene, ROCKET, ASTEROID);
-    scene_add_body(scene, ASTEROID);
+    create_collision_rocket_obstacles_2(scene, rocket, asteroid);
+    scene_add_body(scene, asteroid);
 }
